9f62c96dd6a6ce04a794262a0f1d18f44d6d2782
Marco <marco.carone@libero.it>
2020-01-08 15:54:14 +0100

    FIX JS

--- js/script.js
+++ js/script.js
@@ -1,12 +1,13 @@
-var cognomi = ["carone", "fiore", "lanzi", "baglini"];
-
-var cognomeUtente = prompt("Inserisci il tuo Cognome").toLowerCase();
+// Creo Array lista cognomi
+var cognomi = ["CARONE", "FIORE", "LANZI", "BAGLINI", "RICCIO", "LOGOZZO", "PATTI"];
+// chiedo il cognome all'utente
+var cognomeUtente = prompt("Inserisci il tuo Cognome").toUpperCase();
 while (cognomeUtente.length <= 0) {
   cognomeUtente = prompt("Inserisci il tuo Cognome");
 }
-
-
+// aggiungo il cognome utente nella lista
 cognomi.push(cognomeUtente);
+// ordino la lista
 cognomi.sort();
 for (var i = 0; i < cognomi.length; i++) {
   document.getElementById("lista-cognomi").innerHTML += "<li>" + cognomi[i] + "</li>";
