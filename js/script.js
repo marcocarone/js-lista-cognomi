4c28f3a1a9cb0c4234371ddd89f0943312c951d4
Marco <marco.carone@libero.it>
2020-01-08 16:06:27 +0100

    fine js

--- js/script.js
+++ js/script.js
@@ -12,3 +12,6 @@ cognomi.sort();
 for (var i = 0; i < cognomi.length; i++) {
   document.getElementById("lista-cognomi").innerHTML += "<li>" + cognomi[i] + "</li>";
 }
+// scrivo la posizione del nome utente nella lista
+var posizione = cognomi.indexOf(cognomeUtente) + 1;
+document.getElementById("posizione").innerHTML = "Sei nella posizione numero " + posizione + " della lista.";
